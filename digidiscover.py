c22357c43a0d9a1fc81244fbba8b5c79cb204fdd
Andre Miras <andre.miras@oridao.com>
2014-06-13 16:39:48 +0200

    pep8 fixes:  - W293 blank line contains whitespace  - E501 line too long  - E302 expected 2 blank lines  - E301 expected 1 blank line  - E401 multiple imports on one line

--- digidiscover.py
+++ digidiscover.py
@@ -4,21 +4,27 @@
 # Author: John Kelley  <FirstName> (at) <LastName> dot ca
 # 10/24/08
 
-import socket, signal, traceback
+import socket
+import signal
+import traceback
 from netifaces import interfaces, ifaddresses, AF_INET
 from struct import unpack
+
 
 class SocketTimeOut(Exception):
     def __init__(self, value):
         self.parameter = value
+
     def __str__(self):
         return repr(self.parameter)
 
-def handler(signum,frame):
-    """This is a handler function called when a SIGALRM is received, 
+
+def handler(signum, frame):
+    """This is a handler function called when a SIGALRM is received,
     it simply raises a string exception"""
 
     raise SocketTimeOut(frame)
+
 
 def detectDigiDevice(timeout=1):
     """This sends a UDP broadcast packet out over all available
@@ -29,19 +35,20 @@ def detectDigiDevice(timeout=1):
     listenPort = 1181
     broadcastPort = 2362
     digiDiscoverPacket = "DIGI\x00\x01\x00\x06\xff\xff\xff\xff\xff\xff"
-    
+
     # setup socket
     outsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     outsock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    outsock.bind(('',listenPort))
-    
+    outsock.bind(('', listenPort))
+
     # send our discovery packet out over all interfaces
     for ifaceName in interfaces():
         try:
             for i in ifaddresses(ifaceName)[AF_INET]:
-                outsock.sendto(digiDiscoverPacket, (i['broadcast'], broadcastPort))
+                outsock.sendto(
+                    digiDiscoverPacket, (i['broadcast'], broadcastPort))
         except:
-            True    # for lack of a better way to not care about the exception and just continue
+            pass
 
     responses = []
 
@@ -54,13 +61,13 @@ def detectDigiDevice(timeout=1):
         while True:
             # wait for data
             data, addr = outsock.recvfrom(2048)
-        
+
             # process data
             if not data.startswith('DIGI'):
                 return None
             mac = "%02X:%02X:%02X:%02X:%02X:%02X" % (
-                    ord(data[10]), ord(data[11]), ord(data[12]),
-                    ord(data[13]), ord(data[14]), ord(data[15]))
+                ord(data[10]), ord(data[11]), ord(data[12]),
+                ord(data[13]), ord(data[14]), ord(data[15]))
             len = ord(data[35])
             desc = data[36:(36+len)]+" "
             len2 = ord(data[36+len+7])
